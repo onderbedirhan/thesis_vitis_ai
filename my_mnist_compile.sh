087c3a7cd27cf2226a45d9cacb25029e62c4e45d
onderbedirhan <onderbedirhan00@gmail.com>
2024-03-09 19:22:43 +0300

    Create parametric compiling script

--- my_mnist_compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ARCH=./arch.json
-COMPILE_KV260=./outputs_kv260
-
-vai_c_tensorflow2 \
-    --model      ./quantized_model.h5 \
-    --arch       ./arch.json \
-    --output_dir ./outputs/ \
-    --net_name   my_mnist_test
