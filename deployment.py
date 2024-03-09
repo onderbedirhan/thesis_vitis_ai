2ffcabb80f21c77477c2bdcbba281b621d88c07b
onderbedirhan <onderbedirhan00@gmail.com>
2024-03-09 19:48:34 +0300

    Calculate running time in depleyment.py

--- deployment.py
+++ deployment.py
@@ -1,11 +1,20 @@
+import sys
 import numpy as np
 import cv2
 import vitis_ai_library
 import xir
 from IPython.display import Image
+import time  # Import the time module
+
+# Check if the model name argument is provided
+if len(sys.argv) != 2:
+    print("Usage: python script.py <model_name>")
+    sys.exit(1)
+
+DPU_CONFIG = sys.argv[1]
 
 # file path
-MODEL_PATH = "./outputs/my_mnist_test.xmodel"
+MODEL_PATH = f"./outputs/{DPU_CONFIG}/{DPU_CONFIG}.xmodel"
 IMG_PATH = "./my_mnist_test.jpg"
 
 g = xir.Graph.deserialize(MODEL_PATH)
@@ -30,6 +39,9 @@ image = image.astype(np.int8)
 # set input data
 inputData[0][0] = image.reshape(28, 28, 1)
 
+# Start timing
+start_time = time.time()
+
 # output buffer
 outputData = runner.get_outputs()
 
@@ -37,11 +49,16 @@ outputData = runner.get_outputs()
 job_id = runner.execute_async(inputData, outputData)
 runner.wait(job_id)
 
+end_time = time.time()
+duration_ms = (end_time - start_time) * 1000
+
 resultList = np.asarray(outputData[0])[0]
 resultIdx = resultList.argmax()
 resultVal = resultList[resultIdx]
 print("Predictions")
 for i, x in enumerate(resultList):
     print("%d : %f"%(i, x))
+
+print(f"Prediction time: {duration_ms:.2f} ms")  # Print the time taken in milliseconds
 
 del runner
